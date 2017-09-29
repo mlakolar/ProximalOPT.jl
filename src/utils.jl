f81a3829447d2c18846b85b9070639e896c0f67c
Mladen Kolar <mkolar@gmail.com>
2017-09-29 17:19:20 -0500

    update

--- src/utils.jl
+++ src/utils.jl
@@ -63,3 +63,29 @@ function check_optim_done{T<:AbstractFloat}(iter,
                                             options::ProximalOptions)
   iter >= options.maxiter || abs(curval-lastval) < convert(T, options.ftol) || _l2diff(z, x) < convert(T, options.xtol)
 end
+
+
+
+##############################
+
+macro def(name, definition)
+  esc(quote
+    macro $name()
+      esc($(Expr(:quote, definition)))
+    end
+  end)
+end
+
+@def add_linesearch_fields begin
+    x_ls::Array{T,N}
+    alpha::T
+    mayterminate::Bool
+    lsr::LineSearches.LineSearchResults
+end
+
+@def initial_linesearch begin
+    (similar(initial_x), # Buffer of x for line search in state.x_ls
+    LineSearches.alphainit(one(T), initial_x, gradient(d), value(d)), # Keep track of step size in state.alpha
+    false, # state.mayterminate
+    LineSearches.LineSearchResults(T))
+end
